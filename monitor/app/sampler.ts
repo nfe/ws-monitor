59dadfd544d3e4687f785ea1db152b7bc9df7bf3
Gabriel Marquez <gabriel@conube.com.br>
2015-09-01 22:52:00 -0300

    refactoring sampler

--- monitor/app/sampler.ts
+++ monitor/app/sampler.ts
@@ -12,16 +12,22 @@ import Target = require('./models/target');
 import Sample = require('./models/sample');
 
 class Sampler {
-  private _url : string;
   private _http : HttpClient;
-  private _certificatePath : string;
-  private _certificatePassword : string;
+  private _options : request.Options;
 
   constructor (target : Target) {
-    this._url = target.Url;
-    this._certificatePath = target.CertificatePath;
-    this._certificatePassword = target.CertificatePassword;
     this._http = new HttpClient();
+    this._options = {
+      url: target.Url,
+      timeout: 20000,
+      time: true,
+      agentOptions: {
+        requestCert: true,
+        pfx: fs.readFileSync(target.CertificatePath),
+        passphrase: target.CertificatePassword,
+        rejectUnauthorized: false
+      }
+    };
   }
 
   public sample() : Q.Promise<Sample> {
@@ -30,40 +36,20 @@ class Sampler {
     var start = Date.now();
     sample.Timestamp = new Date(start);
 
-    var options: request.Options = {
-      url: this._url,
-      timeout: 20000,
-      time: true,
-      agentOptions: {
-        requestCert: true,
-        pfx: fs.readFileSync(this._certificatePath),
-        passphrase: this._certificatePassword,
-        rejectUnauthorized: false
-      }
-    };
-
     var self = this;
 
     return Q.Promise<Sample>(function(resolve, reject, notify)
     {
       self._http
-          .get(options)
+          .get(self._options)
           .then(d => {
-
             sample.Elapsed = d.elapsedTime;
             sample.StatusCode = d.statusCode;
 
             resolve(sample); // return promise
           })
           .fail(e => {
-
-            if (e.elapsedTime === null || e.elapsedTime === undefined) {
-              sample.Elapsed = new Date(Date.now() - start).getMilliseconds();
-            }
-            else {
-              sample.Elapsed = e.elapsedTime;
-            }
-
+            sample.Elapsed = 0;
             sample.StatusCode = 500;
 
             resolve(sample); // return promise
